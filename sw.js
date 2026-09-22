a97977f5bab1e8f87ac0d3d9e8dc00c83a63c2c8
sercan-oyun <ankarareport06@gmail.com>
2026-09-22 13:04:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Oyun Salonu: bütün dosyaları cihaza kaydeder, sonra internet olmadan açılır.
+// Uygulamayı güncellerken aşağıdaki sürüm adını değiştirin (ör. v2).
+const CACHE = 'oyun-salonu-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => hit ||
+      fetch(e.request).then(res => {
+        if (res.ok && new URL(e.request.url).origin === location.origin) {
+          const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html')))
+  );
+});
